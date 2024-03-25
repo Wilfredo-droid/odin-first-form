6e6fa39f31ddfd0db5e2d61d665fa2706a7f947a
Wilfredo-droid <wilfredoandres10@gmail.com>
2024-03-25 18:55:25 -0400

    Make a function to match passwords

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let passwordTag = document.querySelector("#password");
+let confirmPasswordTag = document.querySelector("#passwordConfirm");
+
+let passwordMatcher =  (password, confirmPassword) => {
+    if(password == confirmPassword){
+        console.log(1);
+    }
+}
+
