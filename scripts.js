601047bd847ef81db357df2b14cff5a5fed8a476
Wilfredo-droid <wilfredoandres10@gmail.com>
2024-03-25 19:31:36 -0400

    Change match function to work with tags

--- scripts.js
+++ scripts.js
@@ -2,8 +2,13 @@ let passwordTag = document.querySelector("#password");
 let confirmPasswordTag = document.querySelector("#passwordConfirm");
 
 let passwordMatcher =  (password, confirmPassword) => {
-    if(password == confirmPassword){
+    
+    if(password.value == confirmPassword.value){
         console.log(1);
     }
+
+
 }
+
+
 
