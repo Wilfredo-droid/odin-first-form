7f41a1bee152feeb8c9061afd3a79bac3c6821f9
Wilfredo-droid <wilfredoandres10@gmail.com>
2024-04-11 21:15:35 -0400

    Stop appending the error message if it exist

--- scripts.js
+++ scripts.js
@@ -1,19 +1,23 @@
 let passwordTag = document.querySelector("#password");
 let confirmPasswordTag = document.querySelector("#passwordConfirm");
 let submitButton = document.querySelector(".submitButton");
+let pError = document.createElement("p");
+
 
 let passwordMatcher =  (password, confirmPassword, e) => {
     
+
 
     if(password.value != confirmPassword.value){
        
         e.preventDefault();
 
-        let pError = document.createElement("p");
         pError.classList.add("matchError");
         pError.innerText = "Passwords do not match";
 
         passwordTag.parentElement.appendChild(pError);
+
+
 
     }
 
